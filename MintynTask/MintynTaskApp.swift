512141f2bae34ee48b65f0a776fbd7f102605c87
Inyene Etoedia <etoedia@gmail.com>
2025-01-13 13:17:18 -0800

    Initial Commit

---
+++ MintynTask/MintynTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MintynTaskApp.swift
+//  MintynTask
+//
+//  Created by Inyene on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MintynTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
